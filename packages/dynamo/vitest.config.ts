fbea3960ca2281a272dfa7e18e3f02028f147e44
Mike Brant <mikebranttx@gmail.com>
2024-02-09 19:03:03 -0600

    chore: package updates and test/release workflow

---
+++ packages/dynamo/vitest.config.ts
@@ -0,0 +1,28 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    testTimeout: 4000,
+    coverage: {
+      enabled: true,
+      all: true,
+      provider: 'v8',
+      reporter: ['text', 'html', 'clover', 'json', 'text-summary'],
+      reportsDirectory: './coverage',
+      thresholds: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100
+      }
+    },
+    environment: 'node',
+    clearMocks: true,
+    root: './'
+  },
+  logLevel: 'info',
+  esbuild: {
+    sourcemap: 'both'
+  }
+})
